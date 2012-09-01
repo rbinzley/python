b40e5c9c047cd02e4a5a82288146ab02ff591d9e
Rich <rich@freetoast.net>
2012-08-31 20:46:39 -0500

    Initial commit

---
+++ packagePi.py
@@ -0,0 +1,20 @@
+# This script will read in a list of raspberian packages and strip
+# out just the package names.
+
+# suedo code
+# open file
+# find a package name
+# append package name to a list
+# output file
+
+# read ile
+inputFile = "/home/pi/Documents/Packaes-8-25-12"
+rawFile = open(inputFile, "r")
+packageData = rawFile.read()
+rawFile.close()
+
+
+startPosition = packageData.find("Package: ")
+endPosition = packageData.find("Source: ")
+
+pring start
